84bfbc8bb7516eb7a2fc4b1f70efb73ff9a92fbe
Admin <maks.ept1@gmail.com>
2021-12-22 19:32:53 +0500

    Initial commit

---
+++ src/js/files/script.js
@@ -0,0 +1,20 @@
+// Импорт функционала ==============================================================================================================================================================================================================================================================================================================================
+// import { isMobile } from "./functions.js";
+import {formsModules} from "./forms/forms.js";
+
+formsModules
+
+// // region Main
+// document.addEventListener("DOMContentLoaded", () => {
+//   let mainTitle = document.querySelector('.main__title');
+//   let main__desription = document.querySelector('.main__desription');
+//   let main__body = document.querySelector('.main__body');
+//   if( mainTitle && main__desription && main__body) {
+//     setTimeout(()=> {
+//       mainTitle.classList.add('_watcher-view')
+//       main__desription.classList.add('_watcher-view')
+//       main__body.classList.add('_watcher-view')
+//     },300 )
+//   }
+// })
+// // endregion
